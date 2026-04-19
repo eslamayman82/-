5d08fff664a7439bc8d8102adf2a9ad74f688499
Islam Aymen <eslamabdelbary1219@gmail.com>
2026-04-20 01:41:13 +0200

    Add files via upload
    الة حاسبة

---
+++ الة حاسبة.py
@@ -0,0 +1,8 @@
+name1=float(input("enter farist namber : "))
+oper=input("choosean operation  (+,-,*,/,%) :  ")
+name2=float(input("enter scand namber : "))
+
+if oper== "+": 
+    rasult=name1+name2
+    print(name1,"+", name2, "=", rasult)
+    
